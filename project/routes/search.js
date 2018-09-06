1198407c5736ca415530614dffb429738fdcca33
Christopher Berry <christopherberry@Christophers-MacBook-Air.local>
2018-09-06 14:38:29 -0400

    Created pages for routes and styled them slightly.

--- project/routes/search.js
+++ project/routes/search.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-// '/bird-search' will be routed through here
+// This is the route for the main search page
 router.get("/", (req, res) => {
   res.render("search/search");
+});
+
+// This is the route for the observation search page
+router.get("/observation", (req, res) => {
+  res.render("search/observation");
+});
+
+// This is the route for the name search page
+router.get("/name", (req, res) => {
+  res.render("search/name");
+});
+
+// This is the route for the location search page
+router.get("/location", (req, res) => {
+  res.render("search/location");
 });
 
 module.exports = router;
